09ba22094bb8b9781be1686e79fc9c181d08e675
serpil safa <serpilsafa@gmail.com>
2020-11-16 21:47:27 +0000

    internationalized - add language

--- src/main/java/com/safa/webservices/Web/Service/Helloworld/HelloWorldController.java
+++ src/main/java/com/safa/webservices/Web/Service/Helloworld/HelloWorldController.java
@@ -2,11 +2,18 @@ package com.safa.webservices.Web.Service.Helloworld;
 
 
 import com.safa.webservices.Web.Service.Helloworld.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String helloWorld(){
@@ -16,6 +23,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean(new String[]{"Hello World Bean","Hello World Bean 2","Hello World Bean 3"});
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 
 }
--- src/main/java/com/safa/webservices/Web/Service/WebServiceApplication.java
+++ src/main/java/com/safa/webservices/Web/Service/WebServiceApplication.java
@@ -2,6 +2,13 @@ package com.safa.webservices.Web.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class WebServiceApplication {
@@ -9,5 +16,13 @@ public class WebServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebServiceApplication.class, args);
 	}
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.UK);
+		return  localeResolver;
+	}
+
 
 }
