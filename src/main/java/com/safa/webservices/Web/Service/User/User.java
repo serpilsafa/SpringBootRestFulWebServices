cd254033e4fb2f90b1c1de25b779dac05f3b98ad
serpil safa <serpilsafa@gmail.com>
2020-11-17 15:17:31 +0000

    jpa create with data.sql file

--- src/main/java/com/safa/webservices/Web/Service/User/User.java
+++ src/main/java/com/safa/webservices/Web/Service/User/User.java
@@ -3,12 +3,19 @@ package com.safa.webservices.Web.Service.User;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @ApiModel(description = "All Detail about the user")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name size should be latest 2 characters")
